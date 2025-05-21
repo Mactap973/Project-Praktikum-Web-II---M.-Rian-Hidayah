c2a7dbd2e8bdad025f2e27f989bccf9751cd59f3
Mactap973 <ryanhidayah369@gmail.com>
2025-05-21 22:19:36 +0700

    navbar.php

--- navbar.php
+++ navbar.php
@@ -1,16 +1,46 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$username = $_SESSION['username'] ?? null;
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Kursus IT</a>
+        <!-- Brand dan tombol responsif -->
+        <a class="navbar-brand" href="index.php">ITCraft</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
+
+        <!-- Menu -->
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav me-auto">
                 <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="courses.php">Kursus</a></li>
                 <li class="nav-item"><a class="nav-link" href="daftar.php">Daftar Peserta</a></li>
-                <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
                 <li class="nav-item"><a class="nav-link" href="about.php">Profil Pengembang</a></li>
+                <li class="nav-item"><a class="nav-link" href="tentang.php">Tentang Kami</a></li>
+            </ul>
+
+            <!-- Dropdown User -->
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" data-bs-toggle="dropdown">
+                        <span class="ms-1">
+                            <?php echo $username ? htmlspecialchars($username) : 'Akun'; ?>
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <?php if ($username): ?>
+                            <li><a class="dropdown-item" href="profil.php">Profil Saya</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item text-danger" href="logout.php">Logout</a></li>
+                        <?php else: ?>
+                            <li><a class="dropdown-item text-primary" href="login.php">Login</a></li>
+                        <?php endif; ?>
+                    </ul>
+                </li>
             </ul>
         </div>
     </div>
